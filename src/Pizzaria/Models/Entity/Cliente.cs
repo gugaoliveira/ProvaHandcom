3ec45e0f542a144d38429b0f84e74f85409abb04
Tais Bedendo do Valle <taisccoufsj@gmail.com>
2017-08-13 11:50:19 -0300

    Inclusão gitignore e remoção de arquivos desnecessários

---
+++ src/Pizzaria/Models/Entity/Cliente.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pizzaria.Models.Entity
+{
+	/// <summary>
+	/// Cliente da pizzaria
+	/// </summary>
+	public class Cliente : BaseEntity
+    {
+		/// <summary>
+		/// Código do cliente
+		/// </summary>
+        public virtual int Id { get; set; }
+
+		/// <summary>
+		/// Nome do cliente
+		/// </summary>
+        [Display(Name = "Nome Cliente")]
+        public virtual string Nome { get; set; }
+
+		/// <summary>
+		/// CPF do cliente
+		/// </summary>
+        [Display(Name = "CPF")]
+        public virtual string Cpf { get; set; }
+
+		/// <summary>
+		/// Email do cliente
+		/// </summary>
+        [Display(Name = "E-mail")]
+        public virtual string Email { get; set; }
+
+		/// <summary>
+		/// Telefone do cliente
+		/// </summary>
+        [Display(Name = "Telefone")]
+        public virtual string Telefone { get; set; }
+
+		/// <summary>
+		/// Endereco do cliente
+		/// </summary>
+        [Display(Name = "Endereço")]
+        public virtual Endereco Endereco { get; set; }
+
+		/// <summary>
+		/// Lista de pedidos do cliente
+		/// </summary>
+		public virtual List<Pedido> Pedidos { get; set; }
+    }
+}
---
+++ src/Pizzaria/Models/Entity/ItemPedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pizzaria.Models.Entity
+{
+	public class ItemPedido : BaseEntity
+	{
+        /// <summary>
+        /// Código do item
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Quantidade de itens
+        /// </summary>
+        public virtual int Quantidade { get; set; }
+
+        /// <summary>
+        /// Observacao do item
+        /// </summary>
+        public virtual string Observacao { get; set; }
+
+        /// <summary>
+        /// Produto ex.: pizza, bebida ou sobremesa
+        /// </summary>
+        public virtual Produto Produto { get; set; }
+	}
+}
